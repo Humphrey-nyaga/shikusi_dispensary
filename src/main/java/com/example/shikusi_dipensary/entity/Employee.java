679d6d1792c6542d8cac0ee763169d7ee366454b
Humphrey-nyaga <humphreynyaga254@gmail.com>
2023-03-23 21:40:11 +0300

    Include missing entity

---
+++ src/main/java/com/example/shikusi_dipensary/entity/Employee.java
@@ -0,0 +1,9 @@
+package com.example.shikusi_dipensary.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+}
